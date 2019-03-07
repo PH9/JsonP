d824005f14400f02753bcf92b1d5dcb674a0c79f
Wasith Theerapattrathamrong <phai@live.com>
2019-03-07 14:14:11 +0700

    Move call to RequestProtocol

--- JsonP/Model/RequestProtocol.swift
+++ JsonP/Model/RequestProtocol.swift
@@ -1,7 +1,25 @@
+import Alamofire
+
 protocol RequestProtocol {
   associatedtype Response
 
   var path: String { get }
 
   func call(_ result: @escaping (Response) -> Void)
+}
+
+extension RequestProtocol where Response: Decodable {
+  func call(_ result: @escaping (Response) -> Void) {
+    Alamofire.request("https://jsonplaceholder.typicode.com\(path)").responseJSON { (response) in
+      guard let data = response.data else {
+        fatalError("body is nil")
+      }
+
+      guard let users = try? JSONDecoder().decode(Response.self, from: data) else {
+        fatalError("cannot parse json")
+      }
+
+      result(users)
+    }
+  }
 }
--- JsonP/Model/UsersRequest.swift
+++ JsonP/Model/UsersRequest.swift
@@ -1,21 +1,5 @@
-import Alamofire
-
 struct UserRequest: RequestProtocol {
   typealias Response = [User]
 
   let path = "/users"
-
-  func call(_ result: @escaping (Response) -> Void) {
-    Alamofire.request("https://jsonplaceholder.typicode.com\(path)").responseJSON { (response) in
-      guard let data = response.data else {
-        fatalError("body is nil")
-      }
-
-      guard let users = try? JSONDecoder().decode(Response.self, from: data) else {
-        fatalError("cannot parse json")
-      }
-
-      result(users)
-    }
-  }
 }
