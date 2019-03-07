be835dd1aa87386d8ac1fb6c35e62785302d93bf
Wasith Theerapattrathamrong <phai@live.com>
2019-03-07 11:44:57 +0700

    Call users

---
+++ JsonP/Model/UserRequest.swift
@@ -0,0 +1,9 @@
+//
+//  UserRequest.swift
+//  JsonP
+//
+//  Created by Wasith Theerapattrathamrong on 7/3/2562 BE.
+//  Copyright © 2562 Wasith Theerapattrathamrong. All rights reserved.
+//
+
+import Foundation
